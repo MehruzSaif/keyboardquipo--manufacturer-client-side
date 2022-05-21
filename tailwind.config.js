615fadb39554cd93aecb39c907be6cd5d9cc57e8
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-22 04:20:25 +0600

    Navbar and route added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        keyboardTheme: {
+          primary: "#8C80EA",
+          secondary: "#ED7D7E",
+          accent: "#FEBD71",
+          neutral: "#DF51AD",
+          orange: "#F48554",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
