b4d7f7c391d14403c462587795fb7253af942521
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-24 23:33:40 +0600

    my profile and my reviews style changed

--- src/Pages/Dashboard/MyReview.js
+++ src/Pages/Dashboard/MyReview.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MyReview = () => {
     return (
         <div>
-            <h2>My Review</h2>
+            <h2 className='ml-3 mt-2 my-5'>My Review</h2>
         </div>
     );
 };
