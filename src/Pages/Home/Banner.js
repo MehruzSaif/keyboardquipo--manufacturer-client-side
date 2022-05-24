f6fb4c3c069551ce29e0ca188616d06ed39afd62
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-25 02:54:03 +0600

    useToken added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -14,7 +14,7 @@ const Banner = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={keyboard} className="max-w animated" alt=''/>
                 <div>
-                    <h1 className="text-5xl font-bold">Your Next QWERTY</h1>
+                    <h1 className="text-5xl font-bold">Your Next <span className='qwerty'>QWERTY</span></h1>
                     <p className="py-6 font-bold">For mechanical keyboards lovers, by mechanical keyboards lovers. Customize your keyboard to create your perfect aesthetic with an exciting suite of accessories in bold new colorways, including swappable top frames, rotary knobs, keycap sets, and coiled cables!</p>
 
                     <PrimaryButton>GET STARTED</PrimaryButton>
