e4ecc2e5f1ca05e502cae61c9f06d8cf84075eff
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-24 23:30:21 +0600

    tabled added on myOrders and set all info

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -23,37 +23,25 @@ const MyOrders = () => {
 
             <div class="overflow-x-auto">
                 <table class="table w-full">
-                    
+
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>Sl No.</th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Placed Order</th>
                         </tr>
                     </thead>
                     <tbody>
-                        
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        
-                        <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
+                        {
+                            orders.map((order, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{order.buyerName}</td>
+                                <td>{order.partName}</td>
+                            </tr>)
+                        }
+
+
+
                     </tbody>
                 </table>
             </div>
